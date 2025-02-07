235477140e8cb86b43c42486d85018288e6240c2
Talha Sattar <160661345+talhasattar06@users.noreply.github.com>
2025-02-07 11:53:16 +0500

    updated code

---
+++ index.ts
@@ -0,0 +1,47 @@
+#! /usr/bin/env node
+
+import inquirer from 'inquirer'
+import chalk from 'chalk'
+
+class Person {
+    private personality: string;
+    constructor() {
+        this.personality = "Mystery"
+    }
+
+    userInput(input: string) {
+
+        if (input.includes('Enjoy being with people all the time.')) {
+            this.personality = "Extrovert"
+
+        } else if (input.includes('Enjoy spending time alone.')) {
+            this.personality = "Introvert"
+        }
+    }
+    
+    get userPersonality() {
+        return this.personality
+    }
+}
+
+class Main {
+    public async main() {
+        const ans = await inquirer.prompt([
+            {
+                name: 'choice',
+                message: chalk.hex('#7DF9FF')("What do you enjot the most?"),
+                type: 'list',
+                choices: ["Enjoy spending time alone.", "Enjoy being with people all the time."]
+            }
+        ]);
+
+        let myPerson = new Person();
+        myPerson.userInput(ans.choice);
+
+        console.log(`${chalk.hex('#ADD8E6')(`Your are an ${chalk.bgHex('#5D3FD3')(`${myPerson.userPersonality}`)}.`)}`);
+
+    }
+}
+
+const myObject = new Main();
+myObject.main()
